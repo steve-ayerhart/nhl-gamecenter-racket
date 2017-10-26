89756321bb5cba0a8e7b648c78329e5f76afa1cd
Steve Erhart <steve.erhart@dudesolutions.com>
2017-10-26 09:22:17 -0400

    wip refactor

--- nhl-bot-servlet.rkt
+++ nhl-bot-servlet.rkt
@@ -1,39 +1,40 @@
 #lang web-server
 
-(require web-server/servlet
-         web-server/servlet-env
-         web-server/http/xexpr
+(require web-server/servlet-env
+         web-server/dispatch
          json
          web-server/http/request-structs)
 
 (require "nhl.rkt")
 
+(provide nhl-bot current-webhook-url)
+
+(define current-webhook-url (make-parameter #f))
+
 (define (handle-root req)
   (response/xexpr `(html (body "HI"))))
 
-(define (handle-schedule req date-str)
-  (response/xexpr `(html (body ,date-str))))
+(define (handle-events req)
+  (response/xexpr `(html (body "HI"))))
 
-(define (slack-challenge req)
-  (define challenge-data (bytes->jsexpr (request-post-data/raw req)))
-  (response/full
-   200 #"OK" (current-seconds)
-   #"application/json"
-   '()
-   (list (jsexpr->bytes (make-hash `((challenge . ,(hash-ref challenge-data 'challenge))))))))
+(define (slack-event req)
+  (define event-data (bytes->jsexpr (request-post-data/raw req)))
+  (match (string->symbol (hash-ref event-data 'type))
+    (url_verification (response/full
+                       200 #"OK" (current-seconds)
+                       #"application/json"
+                       '()
+                       (list (jsexpr->bytes (make-hash `((challenge . ,(hash-ref challenge-data 'challenge))))))))
+
+    (event_callback (handle-events req))
+
+    (_ (response 400 #"unrecoganized event type" (currernt-seconds) #f '() '()))))
 
 (define-values (nhl-bot-dispatch nhl-bot-url)
   (dispatch-rules
    (("") handle-root)
-   (("slack-challenge") #:method "post" slack-challenge)
+   (("slack-challenge") #:method "post" slack-event)
    (("schedule" (string-arg)) handle-schedule)))
 
 (define (nhl-bot req)
   (nhl-bot-dispatch req))
-
-(serve/servlet nhl-bot
-               #:launch-browser? #f
-               #:port 8080
-               #:servlet-path "/"
-               #:servlet-regexp #rx""
-               #:stateless? #t)
