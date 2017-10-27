d1028ac0b8a72b0dc7cb83d6d684add48b0543f9
Steve Erhart <steve.erhart@gmail.com>
2017-10-26 20:41:28 -0400

    let's use string I guess

--- nhl-bot-servlet.rkt
+++ nhl-bot-servlet.rkt
@@ -17,10 +17,12 @@
   (response/xexpr `(html (body "HI"))))
 
 (define (handle-events req)
+  (displayln (request-post-data/raw req))
   (define-values (status headers in)
-    (http-sendrecv (current-webhook-host) (current-webhook-uri) #:ssl? #t #:method "POST"
-                   (jsexpr->bytes (make-hash '((text . "FART"))))))
-  #t)
+    (http-sendrecv (current-webhook-host) (current-webhook-uri)
+                   #:ssl? #t #:method "POST"
+                   #:data (jsexpr->bytes (make-hash '((text . "IMADUDE"))))))
+  (response/full 200 #"OK (current-seconds" #f '() '()))
 
 (define (slack-events req)
   (define event-data (bytes->jsexpr (request-post-data/raw req)))
@@ -29,10 +31,9 @@
                               (list (jsexpr->bytes (make-hash `((challenge . ,(hash-ref event-data 'challenge))))))))
   (define bad-callback-response (response/full 400 #"unrecognized event type" (current-seconds) #f '() '()))
 
-  (match (string->symbol (hash-ref event-data 'type))
-    (url_verification challenge-response)
-    (event_callback (handle-events req))
-
+  (match (hash-ref event-data 'type)
+    ("url_verification"  challenge-response)
+    ("event_callback" (handle-events req))
     (_ bad-callback-response)))
 
 (define-values (nhl-bot-dispatch nhl-bot-url)
